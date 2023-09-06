aa6f775ae4571e843ac2b60a2ccceb8c2962a7fa
julianp950 <julian.p950@gmail.com>
2023-09-06 17:58:30 -0300

    add polls components

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,10 +1,41 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Button from "../shared/Button.svelte";
 
+  let dispatch = createEventDispatcher();
+
   let fields = { question: "", answerA: "", answerB: "" };
+  let errors = { question: "", answerA: "", answerB: "" };
+  let valid = false;
 
   const submitHandler = () => {
-    // ToDo
+    valid = true;
+
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long.";
+    } else {
+      errors.question = "";
+    }
+
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty.";
+    } else {
+      errors.answerA = "";
+    }
+
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty.";
+    } else {
+      errors.answerB = "";
+    }
+
+    if (valid) {
+      let poll = { ...fields, votesA: 0, votesB: 0, id: Math.random() };
+      dispatch("add", poll);
+    }
   };
 </script>
 
@@ -12,14 +43,17 @@
   <div class="form-field">
     <label for="question">Poll Question: </label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A: </label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B: </label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
@@ -40,5 +74,10 @@
   }
   .form-field {
     margin: 18px auto;
+  }
+  .error {
+    color: #d91b42;
+    font-size: 12px;
+    font-weight: bold;
   }
 </style>
