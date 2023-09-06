a1dc94bb332e3cf47f534aff110a7496ba6d2d8c
julianp950 <julian.p950@gmail.com>
2023-09-06 15:59:34 -0300

    add header footer components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
+
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie
+    tristique turpis id rhoncus. Donec quis tellus non sem venenatis sodales.
+  </h1>
 </main>
 
-<style>
+<Footer />
 
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
