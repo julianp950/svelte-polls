469cb835d5db6245c882de5f28c58eebd513582d
julianp950 <julian.p950@gmail.com>
2023-09-07 19:47:17 -0300

    add polldetails and card component

---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    padding: 20px;
+    border-radius: 6px;
+    background: #fff;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  }
+</style>
